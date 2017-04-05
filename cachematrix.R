5597c3e450fe38464ed2094c07c63c3fc1f30316
Yatish Duhan <yatishduhan@gmail.com>
2017-04-04 22:32:49 -0400

    resubmitting Cachematrix file with adding comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,11 +1,13 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Sometimes to calculate the inverse of matrix can be potentially time consuming operation
+## here created the functions to allow caching of the inverse of matrix 
 
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse
+## have considered to create new matrix by passing matrix properties to the set function of this makeCacheMatrix function
 
 makeCacheMatrix <- function(x = matrix()) {
 	m <- NULL
   set <- function(data = NA, nrow = 1,ncol = 1, byrow = FALSE, dimnames = NULL) {
+  # creating new matrix by allowing user to set the properties leveraging the function parameters.
     x <<- matrix(data = data,nrow = nrow,ncol = ncol, byrow = byrow, dimnames = dimnames)
     m <<- NULL
   }
@@ -17,8 +19,9 @@ makeCacheMatrix <- function(x = matrix()) {
 
 }
 
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
 
-## Write a short comment describing this function
+## If the inverse has already been calculated (and the matrix has not changed), then cacheSolve will retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
@@ -32,3 +35,10 @@ cacheSolve <- function(x, ...) {
   x$setinverse(m)
   m
 }
+
+## to test the above function you can use the following commands:
+## t <- makeCacheMatrix()
+## t$set(1:4,nrow=2,ncol=2)
+## t$get()
+## cacheSolve(t) running first time will create the new inverse matrix
+## running cacheSolve(t) second time will return the inverse matrix from cache and you wil see hte message printed on the command line that fetched from the cache
